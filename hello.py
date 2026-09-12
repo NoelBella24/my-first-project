9783e0b2e034434c734a6d072c279ca57f018e61
Bella <bellaewunkem07@gmail.com>
2026-09-12 04:03:34 +0100

    Fix hobby question

--- hello.py
+++ hello.py
@@ -2,10 +2,11 @@ name = input("What is your name?")
 print("Hello,", name)
 print("Welcome to my first Python project!")
 age = int(input("How old are you?"))
-if age > 18: 
+if age >= 18: 
     print("You are an adult.")
 else:
     print("You are under 18.") 
-if age >= 18:
-    hobby = input("What is your favourite hobby?")
-    print("Nice,",hobby,"sounds fun!")
+
+hobby = input("What is your favourite hobby?")
+print("Nice,",hobby,"sounds fun!") 
+ 
